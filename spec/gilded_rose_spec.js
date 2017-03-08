725f6b719fd019ac2b35bb4b19077a33944ab00e
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-03-08 13:26:26 +0000

    Create passing test for User Story 2: "The Quality of an item is never negative"

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -7,4 +7,9 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(6)
   });
 
+  it("The Quality of an item is never negative", function(){
+    const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gilgedRose.updateQuality();
+    expect(items[0].quality).toEqual(0)
+  })
 });
