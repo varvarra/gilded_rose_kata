768bc5cff8c0b59d3431e33bd848cef2f5d19ce1
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-03-08 13:20:55 +0000

    Properly fix first test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("When seel by date has passed, quality degrades twice as fast", function() {
-    const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("When sell by date has passed, quality degrades twice as fast", function() {
+    const gilgedRose = new Shop([ new Item("foo", 0, 8) ]);
     const items = gilgedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
+    expect(items[0].quality).toEqual(6)
   });
 
 });
